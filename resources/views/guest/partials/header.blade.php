77b590a07051ffce792d3005ef2f73dd7e3c5fc4
ElperdonXV <davidemartinografico@gmail.com>
2022-02-11 16:53:31 +0100

    trying to fix scss

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,3 +1,37 @@
 <header class="container-90">
-    <img src="{{asset('img/dc-logo.png')}}" alt="">
+    <div class="header__nav">
+         <img src="{{asset('img/dc-logo.png')}}" alt="" class="header__logo">
+            <ul class="header__nav__list">
+                <li class="header__nav__list-item">
+                    <a href="">Characters</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="" class="active">Comics</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">Movies</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">Tv</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">Games</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">Collections</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">Videos</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">Fans</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">News</a>
+                </li>
+                <li class="header__nav__list-item">
+                    <a href="">Shop</a>
+                </li>
+            </ul>
+    </div>
 </header>
