a623d8722c7a8494b503bdc62de95aae8933db9d
ElperdonXV <davidemartinografico@gmail.com>
2022-02-13 16:07:59 +0100

    ok

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -21,6 +21,32 @@
             </div>
         </div>
     </div>
+    <div class="bottom">
+          <div class="container-90">
+              <ul>
+                  <li>
+                      <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="Digital comics">
+                      <span>Digital comics</span>
+                  </li>
+                  <li>
+                      <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="Digital comics">
+                      <span>dc merchandise</span>
+                  </li>
+                  <li>
+                      <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Digital comics">
+                      <span>subscription</span>
+                  </li>
+                  <li>
+                      <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Digital comics">
+                      <span>comic shop locator</span>
+                  </li>
+                  <li>
+                      <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="Digital comics">
+                      <span>dc power visa</span>
+                  </li>
+              </ul>
+            </div>
+        </div>
 @endsection
 
 @section('scripts')
--- resources/views/guest/partials/jumbotron.blade.php
+++ resources/views/guest/partials/jumbotron.blade.php
@@ -1,3 +1,7 @@
 <div class="container">
-    <div class="jumbotron"></div>
+    <div class="jumbotron relative">
+        <div class="title">
+            <h2>Current Series</h2>
+        </div>
+    </div>
 </div>
