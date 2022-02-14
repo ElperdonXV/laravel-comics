028acf6e7498ef6d03d217388e754eb4891e98f8
ElperdonXV <davidemartinografico@gmail.com>
2022-02-14 12:43:25 +0100

    adding content

--- resources/views/guest/comic.blade.php
+++ resources/views/guest/comic.blade.php
@@ -6,5 +6,33 @@
 
 @section('content')
 {{-- @dd($comic) --}}
-<h2>{{$comic['title']}}</h2>
+<div class="container-90">
+    <img src="{{$comic['thumb']}}" alt="">
+    <h2>{{$comic['title']}}</h2>
+    <span>{{$comic['price']}}</span>
+    <span>AVAIBLE</span>
+    <button><select name="Check Avaibility" id="">
+        <option value="ciao">Ciao</option>
+        <option value="hello">Hello</option>
+    </select></button>
+    <p>{{$comic['description']}}</p>
+    <div class="credits">
+        <div class="artist">
+            <h2>Artist:</h2>
+            <ul>
+                @foreach($comic['artists'] as $artist)
+                    <li><a href="">{{$artist}}</a></li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="writers">
+            <h2>Writers:</h2>
+            <ul>
+                @foreach($comic['writers'] as $writer)
+                    <li><a href="">{{$writer}}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</div>
 @endsection
